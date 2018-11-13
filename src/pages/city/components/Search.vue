1c8e729d45bf2b3cc05988975e3aaa16fc465228
lkjtzsq <879932397@qq.com>
2018-11-13 15:03:09 +0800

    '添加搜索框'

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="search">
+  <input type="text" class="search-input" placeholder="输入城市名或拼音">
+</div>
+</template>
+
+<script>
+export default {
+  name: "CitySearch"
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "~styles/varibles.styl"
+  .search
+    height:.72rem;
+    padding:0 .1rem;
+    background: $bgColor
+    .search-input
+      width:100%;
+      height:.62rem;
+      line-height: .62rem;
+      text-align: center;
+      border-radius: .06rem;
+      font-size: .3rem;
+      display: block;
+      color:#666;
+      padding: 0 .1rem;
+      box-sizing: border-box;
+</style>
