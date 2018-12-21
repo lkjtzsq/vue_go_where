a8ab37ca8fd888da9abd7e59e0e6985929dbd32c
lkjtzsq <879932397@qq.com>
2018-12-21 11:28:49 +0800

    'search-finish'

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,61 @@
 <template>
-<div class="search">
-  <input type="text" class="search-input" placeholder="输入城市名或拼音">
+<div class="">
+  <div class="search">
+    <input type="text" class="search-input" placeholder="输入城市名或拼音" v-model="keyword">
+  </div>
+  <div class="search-content" ref="search" v-show="keyword.length">
+    <ul>
+      <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+      <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配项目</li>
+    </ul>
+  </div>
 </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: "CitySearch"
+  name: "CitySearch",
+  props: {
+    cities: Object
+  },
+  data() {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed:{
+    hasNoData(){
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if(!this.keyword){
+        this.list=[]
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted(){
+    this.scroll=new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -27,4 +76,19 @@ export default {
       color:#666;
       padding: 0 .1rem;
       box-sizing: border-box;
+
+  .search-content
+    z-index: 1;
+    overflow: hidden;
+    position: absolute;
+    top: 1.58rem;
+    left:0;
+    right:0;
+    bottom: 0;
+    background: #fff;
+    .search-item
+      line-height: .62rem;
+      padding-left: .2rem;
+      color: #666;
+      font-size:.3rem;
 </style>
